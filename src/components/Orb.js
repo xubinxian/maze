e0fb0b8f046d9f2af5f4310400aa4c01fd791b7d
xubinxian <476427590@qq.com>
2020-06-05 21:48:00 +0800

    -

--- src/components/Orb.js
+++ src/components/Orb.js
@@ -13,7 +13,7 @@ export default function Orb(radius, fillColor, canvas) {
     this.xShift = 0;
     this.yShift = 0;
     this.friction = .83;
-    this.speed = 4.85;
+    this.speed = 1.85;
     this.color0 = {
         r: 234,
         g: 203,
@@ -24,36 +24,46 @@ export default function Orb(radius, fillColor, canvas) {
         g: 78,
         b: 32
     };
+    this.color00 = {
+        r: 22,
+        g: 165,
+        b: 145
+    };
+    this.color090 = {
+        r: 242,
+        g: 180,
+        b: 37
+    }
 
     this.update = function () {
         if (this.canvas.orientation) {
             if (canvas.orientation.gamma > 0) {
                 // this.xCoordinate += this.speed;
                 for (let key in this.color0) {
-                    if (this.color0[key] < 255) {
-                        this.color0[key] += 0.1;
+                    if (this.color0[key] < this.color00[key]) {
+                        this.color0[key] += this.speed;
                     }
                 }
-            } else if (canvas.orientation.gamma < 0) {
+            } else  {
                 // this.xCoordinate -= this.speed;
                 for (let key in this.color0) {
-                    if (this.color0[key] > 0) {
-                        this.color0[key] -= 0.1;
+                    if (this.color0[key] > this.color00[key]) {
+                        this.color0[key] -= this.speed
                     }
                 }
             }
             if (canvas.orientation.beta > 0) {
                 // this.yCoordinate += this.speed;
                 for (let key in this.color09) {
-                    if (this.color09[key] < 255) {
-                        this.color09[key] += 0.1;
+                    if (this.color09[key] < this.color090[key]) {
+                        this.color09[key] += this.speed;
                     }
                 }
-            } else if (canvas.orientation.beta < 0) {
+            } else {
                 // this.yCoordinate -= this.speed;
                 for (let key in this.color09) {
-                    if (this.color09[key] > 0) {
-                        this.color09[key] -= 0.1;
+                    if (this.color09[key] > this.color090[key]) {
+                        this.color09[key] -= this.speed;
                     }
                 }
             }
@@ -68,7 +78,6 @@ export default function Orb(radius, fillColor, canvas) {
         // Create gradients  
         var ctx = this.context;
         var radgrad = ctx.createRadialGradient(this.xCoordinate, this.yCoordinate, 0, this.xCoordinate, this.yCoordinate, radius);
-        console.log(this.color0)
         radgrad.addColorStop(0, `rgb(${this.color0.r}, ${this.color0.g}, ${this.color0.b})`);
         radgrad.addColorStop(0.9, `rgb(${this.color09.r}, ${this.color09.g}, ${this.color09.b})`);
         radgrad.addColorStop(1, '#3d3b4f');
